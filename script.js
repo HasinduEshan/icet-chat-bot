71fc5ea6304e0c0db40434ccf0bfb37066aa51e5
Eshan DeSilva <hasindueshandesilva@gmail.com>
2023-10-14 21:51:14 +0530

    stage 3 completed

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function toast() {
   i = 0;
   toastBootstrap.show();
   msg =
-    "Hi there! Welcome to iCET - Institute of Computer Engineering and Technology. I'm Teci, your digital assistant. Please can I know your name?";
+    "Hi there! Welcome to iCET Institute. I'm Teci, your digital assistant. Please can I know your name?";
   writer();
 }
 
@@ -104,35 +104,162 @@ function askEducation() {
     toast_body.appendChild(grt);
 
     //create the input text field element
-    const txt_div = document.createElement("input");
-    txt_div.className = "form-control";
-    txt_div.id = "txtEdu";
-    txt_div.type = "text";
-    txt_div.placeholder = "Please enter your qualification";
+    // const txt_div = document.createElement("input");
+    // txt_div.className = "form-control";
+    // txt_div.id = "txtEdu";
+    // txt_div.type = "text";
+    // txt_div.placeholder = "Please enter your qualification";
+
+    //unorder list items
+
+    const before_ol = document.createElement("a");
+    before_ol.innerHTML = "Before O/L";
+    before_ol.className = "dropdown-item";
+    before_ol.addEventListener("click", () => {
+      askEmployability("Before O/L");
+    });
+    const before_ol_li = document.createElement("li");
+    before_ol_li.appendChild(before_ol);
+
+    const after_ol = document.createElement("a");
+    after_ol.innerHTML = "After O/L";
+    after_ol.className = "dropdown-item";
+    after_ol.addEventListener("click", () => {
+      askEmployability("After O/L");
+    });
+    const after_ol_li = document.createElement("li");
+    after_ol_li.appendChild(after_ol);
+
+    const after_al = document.createElement("a");
+    after_al.innerHTML = "After A/L";
+    after_al.className = "dropdown-item";
+    after_al.addEventListener("click", () => {
+      askEmployability("After A/L");
+    });
+    const after_al_li = document.createElement("li");
+    after_al_li.appendChild(after_al);
+
+    const dip = document.createElement("a");
+    dip.innerHTML = "Diploma";
+    dip.className = "dropdown-item";
+    dip.addEventListener("click", () => {
+      askEmployability("Diploma");
+    });
+    const dip_li = document.createElement("li");
+    dip_li.appendChild(dip);
+
+    const hnd = document.createElement("a");
+    hnd.innerHTML = "HND";
+    hnd.className = "dropdown-item";
+    hnd.addEventListener("click", () => {
+      askEmployability("HND");
+    });
+    const hnd_li = document.createElement("li");
+    hnd_li.appendChild(hnd);
+
+    const ug = document.createElement("a");
+    ug.innerHTML = "Undergraduate (Non IT)";
+    ug.className = "dropdown-item";
+    ug.addEventListener("click", () => {
+      askEmployability("Undergraduate (Non IT)");
+    });
+    const ug_li = document.createElement("li");
+    ug_li.appendChild(ug);
+
+    const ug_it = document.createElement("a");
+    ug_it.innerHTML = "Ungergraduate (IT)";
+    ug_it.className = "dropdown-item";
+    ug_it.addEventListener("click", () => {
+      askEmployability("Undergraduate (IT)");
+    });
+    const ug_it_li = document.createElement("li");
+    ug_it_li.appendChild(ug_it);
+
+    const dg = document.createElement("a");
+    dg.innerHTML = "Non IT Degree";
+    dg.className = "dropdown-item";
+    dg.addEventListener("click", () => {
+      askEmployability("Non IT Degree");
+    });
+    const dg_li = document.createElement("li");
+    dg_li.appendChild(dg);
+
+    const dg_it = document.createElement("a");
+    dg_it.innerHTML = "IT Degree";
+    dg_it.className = "dropdown-item";
+    dg_it.addEventListener("click", () => {
+      askEmployability("IT Degree");
+    });
+    const dg_it_li = document.createElement("li");
+    dg_it_li.appendChild(dg_it);
+
+    const msc = document.createElement("a");
+    msc.innerHTML = "MSc.";
+    msc.className = "dropdown-item";
+    msc.addEventListener("click", () => {
+      askEmployability("Msc.");
+    });
+    const msc_li = document.createElement("li");
+    msc_li.appendChild(msc);
+
+    const above = document.createElement("a");
+    above.innerHTML = "Above Msc.";
+    above.className = "dropdown-item";
+    above.addEventListener("click", () => {
+      askEmployability("Above Msc.");
+    });
+    const above_li = document.createElement("li");
+    above_li.appendChild(above);
+
+    const ul = document.createElement("ul");
+    ul.className = "dropdown-menu";
+    ul.appendChild(before_ol_li);
+    ul.appendChild(after_ol_li);
+    ul.appendChild(after_al_li);
+    ul.appendChild(dip_li);
+    ul.appendChild(hnd_li);
+    ul.appendChild(ug_li);
+    ul.appendChild(ug_it_li);
+    ul.appendChild(dg_li);
+    ul.appendChild(dg_it_li);
+    ul.appendChild(msc_li);
+    ul.appendChild(above_li);
+
+    const btn_drp_dwn = document.createElement("button");
+    btn_drp_dwn.className = "btn btn-secondary dropdown-toggle";
+    btn_drp_dwn.type = "button";
+    btn_drp_dwn.setAttribute("data-bs-toggle", "dropdown");
+    btn_drp_dwn.setAttribute("aria-expanded", "false");
+    btn_drp_dwn.innerHTML = "Please Select";
+
+    const drp_dwn = document.createElement("div");
+    drp_dwn.className = "dropdown";
+    drp_dwn.appendChild(btn_drp_dwn);
+    drp_dwn.appendChild(ul);
 
     //create input text field elemnt holder div
-    const txt_div_holder = document.createElement("div");
-    txt_div_holder.className = "d-flex justify-content-end";
-    txt_div_holder.appendChild(txt_div);
+    const drp_dwn_holder = document.createElement("div");
+    drp_dwn_holder.className = "d-flex justify-content-center";
+    drp_dwn_holder.appendChild(drp_dwn);
 
-    //create submit button element
-    const btn_submit = document.createElement("button");
-    btn_submit.className = "btn btn-outline-primary";
-    btn_submit.type = "button";
-    btn_submit.innerHTML = "Next";
-    btn_submit.addEventListener("click", () => {
-      askEmployability();
-    });
+    // //create submit button element
+    // const btn_submit = document.createElement("button");
+    // btn_submit.className = "btn btn-outline-primary";
+    // btn_submit.type = "button";
+    // btn_submit.innerHTML = "Next";
+    // btn_submit.addEventListener("click", () => {
+    //   askEmployability();
+    // });
 
     //create submit button holder div element
-    const btn_submit_holder = document.createElement("div");
-    btn_submit_holder.className = "d-flex justify-content-end btn-submit";
-    btn_submit_holder.appendChild(btn_submit);
+    // const btn_submit_holder = document.createElement("div");
+    // btn_submit_holder.className = "d-flex justify-content-end btn-submit";
+    // btn_submit_holder.appendChild(btn_submit);
 
     //create the input elements wrapper div
     const input_div = document.createElement("div");
-    input_div.appendChild(txt_div_holder);
-    input_div.appendChild(btn_submit_holder);
+    input_div.appendChild(drp_dwn_holder);
+    // input_div.appendChild(btn_submit_holder);
 
     toast_body.appendChild(input_div);
 
@@ -147,65 +274,64 @@ function askEducation() {
   }
 }
 
-function askEmployability() {
+function askEmployability(edu) {
   i = 0;
-  if (document.getElementById("txtEdu").value != "") {
-    console.log("Education : " + document.getElementById("txtEdu").value);
+  console.log("Education : " + edu);
 
-    msg = "Are you currently employed?";
-    const toast_body = document.getElementById("toast-body");
-    toast_body.innerHTML = "";
+  msg = "Are you currently employed?";
+  const toast_body = document.getElementById("toast-body");
+  toast_body.innerHTML = "";
 
-    //create the msg div element
-    const grt = document.createElement("div");
-    grt.className = "msg";
-    grt.id = "msg";
+  //create the msg div element
+  const grt = document.createElement("div");
+  grt.className = "msg";
+  grt.id = "msg";
 
-    toast_body.appendChild(grt);
+  toast_body.appendChild(grt);
 
-    //create submit button element
-    const btn_yes = document.createElement("button");
-    btn_yes.className = "btn btn-outline-primary";
-    btn_yes.type = "button";
-    btn_yes.innerHTML = "Yes";
-    btn_yes.addEventListener("click", () => {
-      askITRelated();
-    });
+  //create submit button element
+  const btn_yes = document.createElement("button");
+  btn_yes.className = "btn btn-outline-primary";
+  btn_yes.type = "button";
+  btn_yes.innerHTML = "Yes";
+  btn_yes.addEventListener("click", () => {
+    console.log("Employability : Yes");
+    askITRelated();
+  });
 
-    const btn_no = document.createElement("button");
-    btn_no.className = "btn btn-outline-primary";
-    btn_no.type = "button";
-    btn_no.innerHTML = "No";
-    btn_no.addEventListener("click", () => {
-      courses();
-    });
+  const btn_no = document.createElement("button");
+  btn_no.className = "btn btn-outline-primary";
+  btn_no.type = "button";
+  btn_no.innerHTML = "No";
+  btn_no.addEventListener("click", () => {
+    console.log("Employability : No");
+    courses();
+  });
 
-    //create submit button holder div element
-    const btn_submit_holder = document.createElement("div");
-    btn_submit_holder.className = "d-flex justify-content-end btn-submit";
-    btn_submit_holder.appendChild(btn_yes);
-    btn_submit_holder.appendChild(btn_no);
+  //create submit button holder div element
+  const btn_submit_holder = document.createElement("div");
+  btn_submit_holder.className = "d-flex justify-content-end btn-submit";
+  btn_submit_holder.appendChild(btn_yes);
+  btn_submit_holder.appendChild(btn_no);
 
-    //create the input elements wrapper div
-    const input_div = document.createElement("div");
-    input_div.appendChild(btn_submit_holder);
+  //create the input elements wrapper div
+  const input_div = document.createElement("div");
+  input_div.appendChild(btn_submit_holder);
 
-    toast_body.appendChild(input_div);
+  toast_body.appendChild(input_div);
 
-    //create the empty tips div element
-    const tips = document.createElement("div");
-    tips.className = "mt-2 pt-2 border-top tips";
-    tips.id = "tips";
+  //create the empty tips div element
+  const tips = document.createElement("div");
+  tips.className = "mt-2 pt-2 border-top tips";
+  tips.id = "tips";
 
-    toast_body.appendChild(tips);
+  toast_body.appendChild(tips);
 
-    writer();
-  }
+  writer();
 }
 
 function askITRelated() {
   i = 0;
-  console.log("Employability : yes");
 
   msg = "Are you working in the IT Industry?";
   const toast_body = document.getElementById("toast-body");
@@ -224,6 +350,7 @@ function askITRelated() {
   btn_yes.type = "button";
   btn_yes.innerHTML = "Yes";
   btn_yes.addEventListener("click", () => {
+    console.log("IT Related : Yes");
     courses();
   });
 
@@ -232,6 +359,7 @@ function askITRelated() {
   btn_no.type = "button";
   btn_no.innerHTML = "No";
   btn_no.addEventListener("click", () => {
+    console.log("IT Related : No");
     courses();
   });
 
