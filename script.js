9792131ee246b5d287b373f44fb00d720060a7a9
Eshan DeSilva <hasindueshandesilva@gmail.com>
2023-10-17 17:47:03 +0530

    icm and desqualified completed with age filltering

--- script.js
+++ script.js
@@ -6,16 +6,68 @@ var userName = "";
 var age = 0;
 var contact = "";
 var edu = "";
-var msg = "Hello! How can I help you?";
+var isEmp = "No";
+var isIt = "";
+var msg = "Hi there! Welcome to iCET Institute. I'm Teci, your digital assistant. Can I please know your name?";
 
 const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLive);
-function toast() {
-  document.getElementById("msg").innerHTML = "";
+function start() {
+  // document.getElementById("msg").innerHTML = "";
   i = 0;
   toastBootstrap.show();
   msg =
     "Hi there! Welcome to iCET Institute. I'm Teci, your digital assistant. Can I please know your name?";
-  writer();
+    const toast_body = document.getElementById("toast-body");
+    toast_body.innerHTML = "";
+
+    //create the msg div element
+    const grt = document.createElement("div");
+    grt.className = "msg";
+    grt.id = "msg";
+
+    toast_body.appendChild(grt);
+
+    //create the input text field element
+    const txt_div = document.createElement("input");
+    txt_div.className = "form-control";
+    txt_div.id = "txtName";
+    txt_div.type = "text";
+    txt_div.placeholder = "Please enter your name";
+
+    //create input text field elemnt holder div
+    const txt_div_holder = document.createElement("div");
+    txt_div_holder.className = "d-flex justify-content-end";
+    txt_div_holder.appendChild(txt_div);
+
+    //create submit button element
+    const btn_submit = document.createElement("button");
+    btn_submit.className = "btn btn-outline-primary";
+    btn_submit.type = "button";
+    btn_submit.innerHTML = "Next";
+    btn_submit.addEventListener("click", () => {
+      askAge();
+    });
+
+    //create submit button holder div element
+    const btn_submit_holder = document.createElement("div");
+    btn_submit_holder.className = "d-flex justify-content-end btn-submit";
+    btn_submit_holder.appendChild(btn_submit);
+
+    //create the input elements wrapper div
+    const input_div = document.createElement("div");
+    input_div.appendChild(txt_div_holder);
+    input_div.appendChild(btn_submit_holder);
+
+    toast_body.appendChild(input_div);
+
+    //create the empty tips div element
+    const tips = document.createElement("div");
+    tips.className = "mt-2 pt-2 border-top tips";
+    tips.id = "tips";
+
+    toast_body.appendChild(tips);
+
+    writer();
 }
 
 //writting effect
@@ -28,7 +80,64 @@ function writer() {
 }
 
 function sendSMS(contact, sms) {
-  const url = "https://sms.textware.lk:5001/sms/send_sms.php";
+  //url
+  function _0x545a() {
+    const _0x38cc20 = [
+      "5963481nMiRpT",
+      "4062149emYRDf",
+      "3oAXNCN",
+      "12MVFOwt",
+      "11WatqJj",
+      "96670HgdLTR",
+      "11209730KEFvUq",
+      "6HObeUb",
+      "2690872shPfXs",
+      "https://sms.textware.lk:5001/sms/send_sms.php",
+      "223894osqkgQ",
+      "11217CXCoUG",
+    ];
+    _0x545a = function () {
+      return _0x38cc20;
+    };
+    return _0x545a();
+  }
+  function _0x12f5(_0x2a55ea, _0x552a6b) {
+    const _0x545aab = _0x545a();
+    return (
+      (_0x12f5 = function (_0x12f5d6, _0x992189) {
+        _0x12f5d6 = _0x12f5d6 - 0x1a9;
+        let _0x22900d = _0x545aab[_0x12f5d6];
+        return _0x22900d;
+      }),
+      _0x12f5(_0x2a55ea, _0x552a6b)
+    );
+  }
+  const _0x795194 = _0x12f5;
+  (function (_0x195e67, _0x35a9f2) {
+    const _0x4540be = _0x12f5,
+      _0x380f5c = _0x195e67();
+    while (!![]) {
+      try {
+        const _0x2665ec =
+          (-parseInt(_0x4540be(0x1ad)) / 0x1) *
+            (-parseInt(_0x4540be(0x1a9)) / 0x2) +
+          (parseInt(_0x4540be(0x1aa)) / 0x3) *
+            (parseInt(_0x4540be(0x1ae)) / 0x4) +
+          parseInt(_0x4540be(0x1b0)) / 0x5 +
+          (-parseInt(_0x4540be(0x1b2)) / 0x6) *
+            (parseInt(_0x4540be(0x1ac)) / 0x7) +
+          parseInt(_0x4540be(0x1b3)) / 0x8 +
+          -parseInt(_0x4540be(0x1ab)) / 0x9 +
+          (parseInt(_0x4540be(0x1b1)) / 0xa) *
+            (parseInt(_0x4540be(0x1af)) / 0xb);
+        if (_0x2665ec === _0x35a9f2) break;
+        else _0x380f5c["push"](_0x380f5c["shift"]());
+      } catch (_0x168607) {
+        _0x380f5c["push"](_0x380f5c["shift"]());
+      }
+    }
+  })(_0x545a, 0x8dcc8);
+  const url = _0x795194(0x1b4);
 
   // Define the form data as an object
   const _0x29dc69 = _0x9c60;
@@ -111,7 +220,7 @@ function sendSMS(contact, sms) {
   });
 }
 
-function getAge() {
+function askAge() {
   // Get the user's input from the input field
   i = 0;
   userName = document.getElementById("txtName").value;
@@ -181,6 +290,7 @@ function askContact() {
   if (age != "") {
     // You can use userName as needed (e.g., display it in another toast)
     console.log("Age : " + age);
+
     msg = "What is your contact number (Whatsapp) ?";
     const toast_body = document.getElementById("toast-body");
     toast_body.innerHTML = "";
@@ -210,16 +320,18 @@ function askContact() {
     btn_submit.type = "button";
     btn_submit.innerHTML = "Next";
     btn_submit.addEventListener("click", () => {
-      if(age>15 && age< 36){
+      contact = txt_div.value;
+      if (contact != "" && age > 14 && age < 36) {
         askEducation();
-      }else if(age>11 && age<16){
+      } else if (contact != "" && age > 11 && age < 15) {
+        console.log("Contact : " + contact);
         icm();
-        // sendSMS(
-        //   "94712272272",
-        //   "Name : " + userName + "\nContact : " + contact + "\nAge : " +age
-        // );
-      }else{
-        disqualified()
+        sendSMS(
+          contact,
+          "Thank you for your inquiry.We will contact you soon.For more info please call or WhatsApp on 0705805805" //115
+        );
+      } else if (contact != "") {
+        disqualified();
       }
     });
 
@@ -250,7 +362,6 @@ function askEducation() {
   i = 0;
 
   if (document.getElementById("txtContact").value != "") {
-
     contact = document.getElementById("txtContact").value;
 
     console.log("Contact : " + contact);
@@ -269,14 +380,14 @@ function askEducation() {
     //create data list
     const list = [
       "Before O/L",
-      "After O/L",
-      "After A/L",
+      "O/L",
+      "A/L",
       "Diploma",
       "HND",
       "Undergraduate (Non IT)",
       "Undergraduate (IT)",
-      "Degree (Non IT)",
-      "Degree (IT)",
+      "Graduate (Non IT)",
+      "Graduate (IT)",
       "Msc.",
       "Above Msc.",
     ];
@@ -362,6 +473,7 @@ function askEmployability() {
   btn_yes.innerHTML = "Yes";
   btn_yes.addEventListener("click", () => {
     console.log("Employability : Yes");
+    isEmp = "Yes";
     askITRelated();
   });
 
@@ -370,7 +482,8 @@ function askEmployability() {
   btn_no.type = "button";
   btn_no.innerHTML = "No";
   btn_no.addEventListener("click", () => {
-    console.log("Employability : No");
+    // console.log("Employability : No");
+    isEmp = "No";
     courses();
   });
 
@@ -398,6 +511,7 @@ function askEmployability() {
 
 function askITRelated() {
   i = 0;
+  console.log("Employability : " + isEmp);
 
   msg = "Are you working in the IT Industry?";
   const toast_body = document.getElementById("toast-body");
@@ -416,7 +530,8 @@ function askITRelated() {
   btn_yes.type = "button";
   btn_yes.innerHTML = "Yes";
   btn_yes.addEventListener("click", () => {
-    console.log("IT Related : Yes");
+    // console.log("IT Related : Yes");
+    isIt = "Yes";
     courses();
   });
 
@@ -425,7 +540,8 @@ function askITRelated() {
   btn_no.type = "button";
   btn_no.innerHTML = "No";
   btn_no.addEventListener("click", () => {
-    console.log("IT Related : No");
+    // console.log("IT Related : No");
+    isIt = "No";
     courses();
   });
 
@@ -452,21 +568,110 @@ function askITRelated() {
 }
 
 function courses() {
-  console.log("courses");
-  if(contact!=""){
+  console.log("IT Related : " + isIt);
+  // console.log("courses");
+  if (contact != "") {
     sendSMS(
       contact,
-      "Thank you for inquiring about iCET. We will contact you promptly.\nFor more info please call or WhatsApp 0705805805"  //115
+      "Thank you for your inquiry.We will contact you soon.For more info please call or WhatsApp on 0705805805" //115
     );
   }
 }
 
-function icm(){
+function icm() {
+  // Get the user's input from the input field
+  i = 0;
+  // You can use userName as needed (e.g., display it in another toast)
 
+  msg =
+    "Congratulations! You are eligible for our ICM program. One of our Coordinators will contact you soon.";
+  const toast_body = document.getElementById("toast-body");
+  toast_body.innerHTML = "";
+
+  //create the msg div element
+  const grt = document.createElement("div");
+  grt.className = "msg";
+  grt.id = "msg";
+
+  toast_body.appendChild(grt);
+
+  //create submit button element
+  const btn_submit = document.createElement("button");
+  btn_submit.className = "btn btn-outline-primary";
+  btn_submit.type = "button";
+  btn_submit.innerHTML = "Contact";
+  btn_submit.addEventListener("click", () => {
+    console.log("call or whatsApp");
+  });
+
+  //create submit button holder div element
+  const btn_submit_holder = document.createElement("div");
+  btn_submit_holder.className = "d-flex justify-content-end btn-submit";
+  btn_submit_holder.appendChild(btn_submit);
+
+  //create the input elements wrapper div
+  const input_div = document.createElement("div");
+  input_div.appendChild(btn_submit_holder);
+
+  toast_body.appendChild(input_div);
+
+  //create the empty tips div element
+  const tips = document.createElement("div");
+  tips.className = "mt-2 pt-2 border-top tips";
+  tips.id = "tips";
+  tips.innerHTML = "terms and conditions apply";
+
+  toast_body.appendChild(tips);
+
+  writer();
 }
 
-function disqualified(){
-  
+function disqualified() {
+  // Get the user's input from the input field
+  i = 0;
+  // You can use userName as needed (e.g., display it in another toast)
+
+  msg =
+    "Thank you for inquiring about iCET Software Engineering Training Program. Currently we are unable to find a program suits you. Please call us on 0705805805 for further clarifications.";
+  const toast_body = document.getElementById("toast-body");
+  toast_body.innerHTML = "";
+
+  //create the msg div element
+  const grt = document.createElement("div");
+  grt.className = "msg";
+  grt.id = "msg";
+
+  toast_body.appendChild(grt);
+
+  //create submit button element
+  const btn_submit = document.createElement("button");
+  btn_submit.className = "btn btn-outline-primary";
+  btn_submit.type = "button";
+  btn_submit.innerHTML = "Contact";
+  btn_submit.addEventListener("click", () => {
+    console.log("call or whatsApp");
+  });
+
+  //create submit button holder div element
+  const btn_submit_holder = document.createElement("div");
+  btn_submit_holder.className = "d-flex justify-content-end btn-submit";
+  btn_submit_holder.appendChild(btn_submit);
+
+  //create the input elements wrapper div
+  const input_div = document.createElement("div");
+  input_div.appendChild(btn_submit_holder);
+
+  toast_body.appendChild(input_div);
+
+  //create the empty tips div element
+  const tips = document.createElement("div");
+  tips.className = "mt-2 pt-2 border-top tips";
+  tips.id = "tips";
+  tips.innerHTML = "terms and conditions apply";
+
+  toast_body.appendChild(tips);
+
+  writer();
 }
 
 //set faid-in active of starting options
@@ -517,11 +722,11 @@ function disqualified(){
 //   }
 
 function reload() {
-  if(contact!=""){
-    sendSMS(
-      contact,
-      "Thank you for inquiring about iCET. We will contact you promptly.\nFor more info please call or WhatsApp 0705805805"  //115
-    );
-  }
+  // if (contact != "") {
+  //   sendSMS(
+  //     contact,
+  //     "Thank you for your inquiry.We will contact you soon.For more info please call or WhatsApp on 0705805805" //115
+  //   );
+  // }
   location.reload();
 }
